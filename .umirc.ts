0277216ddfacd7dc507ec231fae6426977e3e4d4
promotion-xu <17521337708@163.com>
2020-11-15 00:52:34 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,40 +1,35 @@
-import { IConfig } from 'umi-types';
+import { IConfig } from 'umi-types'
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   routes: [
     {
       path: '/',
       component: '../layouts/index',
       routes: [
-        { path: '/', component: '../pages/index' }
+        { exact: true, path: '/index', component: '../pages/index' },
+        { exact: true, path: '/tenant', component: '../pages/tenant' }
       ]
     }
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'tenant-management',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: { webpackChunkName: true },
+        title: 'tenant-management',
+        dll: false,
+
+        routes: {
+          exclude: [/models\//, /services\//, /model\.(t|j)sx?$/, /service\.(t|j)sx?$/, /components\//]
+        }
+      }
+    ]
+  ]
 }
 
-export default config;
+export default config
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,86 @@
-import React from 'react';
-import styles from './index.css';
+import React, { useState } from 'react';
+import { Link } from 'umi'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import styles from './index.less';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 const BasicLayout: React.FC = props => {
+  const [collapsed, setCollapsed] = useState<boolean>(false)
+  const onCollapse = (collapsed: boolean) => {
+    setCollapsed(collapsed);
+  }
+
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
+    <Layout className={styles.pageLayout} style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <Link to="/index"><span>index</span></Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="desktop" />
+            <Link to="/tenant"><span>tenant</span></Link>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="user" />
+                <span>User</span>
+              </span>
+            }
+          >
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="team" />
+                <span>Team</span>
+              </span>
+            }
+          >
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9">
+            <Icon type="file" />
+            <span>File</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+            {props.children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>@DFocus tenant-management</Footer>
+      </Layout>
+    </Layout>
   );
 };
 
